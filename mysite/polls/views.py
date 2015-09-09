b181dc75baa0775c9e1891c0f5976ae5ae6c2638
yogesh <yogeshise@gmail.com>
2015-09-09 18:16:47 +0000

    adding more exmple routes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    
+    latest = Question.objects.order_by('-pub_date')[:5]
+    output = ','.join([p.question_text for p in latest])
+    return HttpResponse(output)
+    
+def results(request, question_id):
+    return HttpResponse("you are looking at the result of %s question" %question_id)
